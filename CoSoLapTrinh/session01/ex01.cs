34852af8e66b60a9cf14caed3a460483f04c8ea7
Thúy Ngân <thuyngantngl@gmail.com>
2026-08-27 12:44:59 +0700

    update ss1

---
+++ CoSoLapTrinh/session01/ex01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoSoLapTrinh.session04
+{
+    internal class ex01
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a simple program.");
+            Console.WriteLine("My name is Thuy Ngan")
+        }
+    }
+}
